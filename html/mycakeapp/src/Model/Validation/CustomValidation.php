5402908b700545f7d782889dcbdd211e1b636fbb
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-12 11:09:35 +0900

    fix: 拡張子チェックのエラー文が出るよう修正

--- html/mycakeapp/src/Model/Validation/CustomValidation.php
+++ html/mycakeapp/src/Model/Validation/CustomValidation.php
@@ -17,8 +17,8 @@ class CustomValidation extends Validation
     {
         $check_array = array(1 => 'png', 2 => 'jpeg', 3 => 'jpg', 4 => 'gif');
         $Extension = mb_strtolower($value);
-        if (array_search($Extension, $check_array)) {
-            return true;
+        if (!array_search($Extension, $check_array)) {
+            return false;
         }
     }
 }
