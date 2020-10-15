850fdab4be62213cdf832adc2aaa129e0c811bd1
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-15 12:14:25 +0900

    add: ratingsテーブルのモデル追加

---
+++ html/mycakeapp/src/Model/Table/RatingsTable.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Ratings Model
+ *
+ * @property \App\Model\Table\BiditemsTable&\Cake\ORM\Association\BelongsTo $Biditems
+ * @property \App\Model\Table\AppraiseesTable&\Cake\ORM\Association\BelongsTo $Appraisees
+ * @property \App\Model\Table\ReviewersTable&\Cake\ORM\Association\BelongsTo $Reviewers
+ *
+ * @method \App\Model\Entity\Rating get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Rating newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Rating[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Rating|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Rating saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Rating patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Rating[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Rating findOrCreate($search, callable $callback = null, $options = [])
+ */
+class RatingsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('ratings');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Biditems', [
+            'foreignKey' => 'biditem_id',
+            'joinType' => 'INNER',
+        ]);
+        $this->belongsTo('Users', [
+            'foreignKey' => 'appraisee_id',
+            'joinType' => 'INNER',
+        ]);
+        $this->belongsTo('Users', [
+            'foreignKey' => 'reviewer_id',
+            'joinType' => 'INNER',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->integer('rating_scale')
+            ->requirePresence('rating_scale', 'create')
+            ->notEmptyString('rating_scale');
+
+        $validator
+            ->scalar('rating_comment')
+            ->maxLength('rating_comment', 1000)
+            ->requirePresence('rating_comment', 'create')
+            ->notEmptyString('rating_comment');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['biditem_id'], 'Biditems'));
+
+        return $rules;
+    }
+}
